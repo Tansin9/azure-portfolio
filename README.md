686653b076ba9ea2995d8f06ed55f9b7b4f67199
unknown <pawartanishq007@gmail.com>
2023-11-02 23:16:02 +0530

    function is added

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # azure-portfolio
 My own azure portfolio
 
+# components 
+- azure blob
+- azure function
+- azure cosmos db
 ## Section 1: Building the frontend
 - Created html page to contain Resume info
 - implemented javascript code for the visitor counter
@@ -9,3 +13,11 @@ My own azure portfolio
 - setting up our cosmos DB resources ( creating cosmos DB account, database, and container and data)
 - Setting up Azure function (We'll create Azure Function to interact with Cosmos DB counter data)
 - test locally (we'll test our function locally and make sure we can view our counter data in the browser and in our website locally)
+
+## Section 3: Deploying to Azure
+- Deploy our Azure Function
+We'll deploy our azure function to azure, grab its url and update our javascript code with it
+- deploy to blob container
+we'll deploy our static site to our blob container
+- setup Azure cdn
+we'll setup CDN for HTTPS and custom domain support
--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -1,41 +1,97 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+
 using Microsoft.AspNetCore.Http;
+
+using Microsoft.Azure.WebJobs;
+
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System.Net;
+
+using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
+
 using System.Net.Http;
+
 using System.Text;
 
+using Newtonsoft.Json;
+
+using System.Threading.Tasks;
+
+
+
 namespace Company.Function
+
 {
+
     public static class GetResumeCounter
+
     {
+
         [FunctionName("GetResumeCounter")]
-        public static HttpResponseMessage Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"CloudResume", collectionName: "Counter",
-                ConnectionStringSetting = "CloudResume", Id = "index", PartitionKey = "index")] Counter counter,
-                [CosmosDB(databaseName:"CloudResume", collectionName: "Counter",
-                ConnectionStringSetting = "CloudResume", Id = "index", PartitionKey = "index")] out Counter updatedCounter,
+
+        public static async Task<HttpResponseMessage> Run(
+
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+
+            [CosmosDB(
+
+                databaseName: "AzureResume",
+
+                containerName: "Counter",
+
+                Connection = "AzureResumeConnectionString",
+
+                Id = "1",
+
+                PartitionKey = "1")] Counter counter,
+
+            [CosmosDB(
+
+                databaseName: "AzureResume",
+
+                containerName: "Counter",
+
+                Connection = "AzureResumeConnectionString",
+
+                Id = "1",
+
+                PartitionKey = "1")] IAsyncCollector<Counter> updatedCounter,
+
             ILogger log)
+
         {
 
-            log.LogInformation("GetResumeCounter was triggered.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            updatedCounter = counter;
-            updatedCounter.Count += 1;
+
+
+            // Increment the count
+
+            counter.Count++;
+
+
+
+            // Update the counter in Cosmos DB
+
+            await updatedCounter.AddAsync(counter);
+
+
 
             var jsonToReturn = JsonConvert.SerializeObject(counter);
 
-            return new HttpResponseMessage(HttpStatusCode.OK)
+
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+
             {
+
                 Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
+
             };
+
         }
+
     }
+
 }
